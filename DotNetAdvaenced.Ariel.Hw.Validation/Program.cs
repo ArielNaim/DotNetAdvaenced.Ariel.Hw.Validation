261532b03a208447cee5a58f9b4fd052cfaaf9ce
arieln137@gmail.com <arieln137@gmail.com>
2021-09-28 14:49:49 +0300

    Add project files.

---
+++ DotNetAdvaenced.Ariel.Hw.Validation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DotNetAdvaenced.Ariel.Hw.Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string matche = "foacv";
+            string longchack = "J123456_NM";
+            string zzz = "aayyyeee";
+            string firstAndLastName = "Ariel Naim";
+            double decimalNumber = 31.56;
+            string mail = "137@gmail.com";
+            //matche.IsSratD();
+            //matche.StartWithDFJ();
+            //Console.WriteLine(matche.IsSratD()); // chack for targil 1 
+            //Console.WriteLine(longchack.StartWithDFJ()); // chack for targil 2 +3
+            //Console.WriteLine(zzz.ExchangeYyy());// check for targil 4
+            //Console.WriteLine(matche.StartWitCapital()); // check for targil 5
+            //Console.WriteLine(matche.ContaionAbc()); // check for targil 6
+            //Console.WriteLine(firstAndLastName.FirsAndLastName()); // check for targil 7
+            //Console.WriteLine(decimalNumber.IsDecimalNumber()); // check for targil 8 + 9
+            Console.WriteLine(mail.IsValidEmail()); // check for targil 10
+
+
+        }
+    }
+}
---
+++ DotNetAdvaenced.Ariel.Hw.Validation/ValidationForBigenners.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DotNetAdvaenced.Ariel.Hw.Validation
+{
+    public static class ValidationForBigenners
+    {
+        #region Targil 1 Validation
+        public static bool IsSratD(this string id)
+        {
+            var expression = @"^D.*";
+            return new Regex(expression).IsMatch(id);
+        }
+        #endregion
+        #region Targil 2 + 3 Validation
+        public static bool StartWithDFJ(this string str)
+        {
+            Regex expression = new Regex(@"^[DFJ]\d{6}|\c_\D{2}");
+            return expression.IsMatch(str);
+        }
+        #endregion
+        #region Targil 4 
+        public static string ExchangeYyy(this string str)
+        {
+            Regex expressionZ = new Regex(@"זזזז");
+            Regex expressionY = new Regex(@"yyy");
+            string newString;
+            if (expressionZ.IsMatch(str))
+            {
+                newString = str.Replace('ז', ' ');
+                return newString;
+            }
+            if (expressionY.IsMatch(str))
+            {
+                newString = str.Replace('y', ' ');
+                return newString;
+            }
+            return str;
+        }
+        #endregion
+
+        #region Trgil 5 
+        public static bool StartWitCapital(this string str)
+        {
+            Regex expression = new Regex(@"^[A-Z]");
+            return expression.IsMatch(str);
+        }
+        #endregion
+
+        #region Trgil 6
+        public static bool ContaionAbc(this string str)
+        {
+            Regex expression = new Regex(@"ABC|abc");
+            return expression.IsMatch(str);
+        }
+        #endregion
+
+        #region Targil 7
+        public static bool FirsAndLastName(this string str)
+        {
+            Regex expression = new Regex(@"\D{2,18} \D{2,18}");
+            return expression.IsMatch(str);
+        }
+        #endregion
+        #region Targil 8 + 9
+        public static bool IsDecimalNumber(this double doub)
+        {
+            Regex expression = new Regex(@"^\d+.\d{3}");
+            return expression.IsMatch(doub.ToString());
+        }
+        #endregion
+        #region Targil 10
+        public static bool IsValidEmail(this string email)
+        {
+            Regex expression = new Regex(@"^.+(@)+((yahoo)|(gmail))+.+(com)");
+            return expression.IsMatch(email);
+        }
+        #endregion
+    }
+}
